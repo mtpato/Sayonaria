bce29c1d8870d7d03b0770bff1dd0d964e19b793
Ian Pytlarz <Ian2400@gmail.com>
2012-09-14 01:16:52 -0400

    Starting to work on the new loading view

--- SayonariaGame/LoadingView.m
+++ SayonariaGame/LoadingView.m
@@ -57,35 +57,6 @@
     return loaderView;
 }
 
-- (UIImage *)addBackground{
-	// Create an image context (think of this as a canvas for our masterpiece) the same size as the view
-    UIGraphicsBeginImageContextWithOptions(self.bounds.size, YES, 1);
-	// Our gradient only has two locations - start and finish. More complex gradients might have more colours
-    size_t num_locations = 2;
-	// The location of the colors is at the start and end
-    CGFloat locations[2] = { 0.0, 1.0 };
-	// These are the colors! That's two RBGA values
-    CGFloat components[8] = {
-        0.4,0.4,0.4, 0.8,
-        0.1,0.1,0.1, 0.5 };
-	// Create a color space
-    CGColorSpaceRef myColorspace = CGColorSpaceCreateDeviceRGB();
-	// Create a gradient with the values we've set up
-    CGGradientRef myGradient = CGGradientCreateWithColorComponents (myColorspace, components, locations, num_locations);
-	// Set the radius to a nice size, 80% of the width. You can adjust this
-    float myRadius = (self.bounds.size.width*.8)/2;
-	// Now we draw the gradient into the context. Think painting onto the canvas
-    CGContextDrawRadialGradient (UIGraphicsGetCurrentContext(), myGradient, self.center, 0, self.center, myRadius, kCGGradientDrawsAfterEndLocation);
-	// Rip the 'canvas' into a UIImage object
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-	// And release memory
-    CGColorSpaceRelease(myColorspace);
-    CGGradientRelease(myGradient);
-    UIGraphicsEndImageContext();
-	// … obvious.
-    return image;
-}
-
 -(void)removeLoader:(UIView *)superView {
     CATransition *animation = [CATransition animation];
 	[animation setType:kCATransitionFade];
