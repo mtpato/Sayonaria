7c3e8d763bf493b9a3097f5b2039c38b47089909
Ian Pytlarz <Ian2400@gmail.com>
2012-11-01 18:51:36 -0400

    Added a class to control the options for a new game

---
+++ SayonariaGame/SayonariaGame/NewGameOptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewGameOptionsViewController.h
+//  SayonariaGame
+//
+//  Created by Ian Pytlarz on 11/1/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewGameOptionsViewController : UIViewController
+
+@end
---
+++ SayonariaGame/SayonariaGame/NewGameOptionsViewController.m
@@ -0,0 +1,43 @@
+//
+//  NewGameOptionsViewController.m
+//  SayonariaGame
+//
+//  Created by Ian Pytlarz on 11/1/12.
+//
+//
+
+#import "NewGameOptionsViewController.h"
+
+@interface NewGameOptionsViewController ()
+
+@end
+
+@implementation NewGameOptionsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
